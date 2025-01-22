df885abe9172c997ae26f2c6785652caff9377d1
arashjp8 <arashjafarpour723@gmail.com>
2025-01-22 15:40:44 +0330

    [refactor] - using spawn to run ffmpeg directly from nodejs app

--- src/video-stream/video-stream.service.ts
+++ src/video-stream/video-stream.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { Response } from "express";
-import * as ffmpeg from "fluent-ffmpeg";
+import { spawn } from "child_process";
 import * as fs from "fs";
+import * as path from "path";
 
 @Injectable()
 export class VideoStreamService {
   streamVideo(filePath: string, res: Response): void {
-    if (!fs) {
+    const absolutePath = path.resolve(filePath);
+    if (!fs.existsSync(absolutePath)) {
       res.status(404).send("Video file not found!");
       return;
     }
@@ -14,20 +16,37 @@ export class VideoStreamService {
     res.setHeader("Content-Type", "vide/webm");
     res.setHeader("Accept-Ranges", "bytes");
 
-    ffmpeg(filePath)
-      .videoCodec("libx264")
-      .audioCodec("aac")
-      .format("matroska")
-      .on("start", (commandLine) => {
-        console.log("FFmpeg command: ", commandLine);
-      })
-      .on("error", (err) => {
-        console.error("FFmpeg error: ", err);
-        res.status(500).send("Error streaming video");
-      })
-      .on("end", () => {
+    const ffmpegCommand = spawn("ffmpeg", [
+      // input file
+      "-i",
+      absolutePath,
+      // video codec
+      "-c:v",
+      "libx264",
+      // audio codec
+      "-c:a",
+      "aac",
+      // output format
+      "-f",
+      "matroska",
+      // pipe the output to stdout
+      "pipe:1",
+    ]);
+
+    // pipe ffmpeg stdout to http response
+    ffmpegCommand.stdout.pipe(res);
+
+    ffmpegCommand.on("error", (err) => {
+      console.error("FFmpeg error: ", err);
+      res.status(500).send("Error streaming video");
+    });
+
+    ffmpegCommand.on("close", (code) => {
+      if (code === 0) {
         console.log("Streaming finished.");
-      })
-      .pipe(res, { end: true });
+      } else {
+        console.log(`FFmpeg process exited with code ${code}`);
+      }
+    });
   }
 }
